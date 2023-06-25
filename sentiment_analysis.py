669300c3223dd8d8f2c362ebeec25563dd0e2439
msbeigi <msbeigi@gmail.com>
2023-06-25 18:43:34 +0100

    add black command

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,6 +1,8 @@
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
-nltk.download('vader_lexicon')
+
+nltk.download("vader_lexicon")
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -8,11 +10,11 @@ def analyze_sentiment(text):
     """Analyze the sentiment of the given text and return the sentiment score."""
     sid = SentimentIntensityAnalyzer()
     sentiment_scores = sid.polarity_scores(text)
-    compound_score = sentiment_scores['compound']
+    compound_score = sentiment_scores["compound"]
 
     if compound_score >= 0.05:
-        return 'Positive'
+        return "Positive"
     elif compound_score <= -0.05:
-        return 'Negative'
+        return "Negative"
     else:
-        return 'Neutral'
+        return "Neutral"
